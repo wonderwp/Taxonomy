f837af819fd06be784676979f659e7f8fc12e0c7
Jeremy Desvaux <jeremy@jdmweb.com>
2024-11-28 14:46:26 +0100

    doc(interface): improve php doc

--- src/Traits/HasCustomTypeDefinitionsInterface.php
+++ src/Traits/HasCustomTypeDefinitionsInterface.php
@@ -2,8 +2,28 @@
 
 namespace WonderWp\Component\Taxonomy\Traits;
 
+use WonderWp\Component\Taxonomy\Definition\TaxonomyInterface;
+
 interface HasCustomTypeDefinitionsInterface
 {
+    /**
+     * Provide the key of the taxonomy
+     * @see TaxonomyInterface::setKey()
+     * @return string
+     */
     public static function provideKey(): string;
+
+    /**
+     * Provide the object types of the taxonomy
+     * @see TaxonomyInterface::setObjectTypes()
+     * @return array
+     */
+    public static function provideObjectTypes(): array;
+
+    /**
+     * Provide the args of the taxonomy
+     * @see TaxonomyInterface::setArgs()
+     * @return array
+     */
     public static function provideArgs(): array;
 }
