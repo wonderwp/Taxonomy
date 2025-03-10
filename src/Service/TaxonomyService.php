ac6b0eae6e64450a0939618ea7df880d65df6d7f
Jeremy Desvaux <jeremy@jdmweb.com>
2025-03-10 11:12:08 +0100

    fix(autoload): fix interface reference

--- src/Service/TaxonomyService.php
+++ src/Service/TaxonomyService.php
@@ -5,6 +5,7 @@ namespace WonderWp\Component\Taxonomy\Service;
 use WonderWp\Component\PluginSkeleton\ManagerAwareInterface;
 use WonderWp\Component\PluginSkeleton\ManagerAwareTrait;
 use WonderWp\Component\PluginSkeleton\Service\RegistrableInterface;
+use WonderWp\Component\Taxonomy\Definition\TaxonomyInterface;
 
 class TaxonomyService extends AbstractTaxonomyService implements RegistrableInterface, ManagerAwareInterface
 {
